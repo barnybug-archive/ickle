78495d0d7fc5b3805595eee7136c51404e4bb28f
cborni <>
2003-06-30 20:06:27 +0000

    damn, I forgot the most important files.....

---
+++ ickle/FindTextDialog.cpp
@@ -0,0 +1,73 @@
+/*
+ * FindTextDialog
+ * Copyright (C) 2001 Barnaby Gray <barnaby@beedesign.co.uk>.
+ * Copyright (C) 2003 Christian Borntraeger <linux@borntraeger.net>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "FindTextDialog.h"
+
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/label.h>
+#include <gtkmm/main.h>
+#include <gtkmm/stock.h>
+#include <gtkmm/buttonbox.h>
+#include <gtkmm/entry.h>
+
+#include "ickle.h"
+
+using std::string;
+
+FindTextDialog::FindTextDialog(Gtk::Window& parent, const Glib::ustring title,
+    const Glib::ustring question, const Glib::ustring oldsearch="")
+    : Gtk::Dialog(title, false, false)
+{
+  Gtk::VBox *vbox=get_vbox();
+
+  m_case_sensitive.set_label(_("case sensitve"));
+  m_case_sensitive.set_active(false);
+
+  vbox->pack_start(m_case_sensitive);
+
+  m_tooltip.set_tip (m_search_text,_("What text are you looking for?"));
+  m_search_text.set_text(oldsearch);
+  vbox->pack_start(m_search_text);
+
+  add_button(Gtk::Stock::CLOSE, Gtk::RESPONSE_CLOSE);
+  add_button( _("_Find"), RESPONSE_FIND);
+  set_default_response(RESPONSE_FIND);
+
+  set_position(Gtk::WIN_POS_CENTER);
+  set_default_size( 250, 100 );
+  set_border_width(10);
+  set_transient_for(parent);
+  show_all();
+}
+
+FindTextDialog::~FindTextDialog()
+{
+}
+
+void FindTextDialog::on_response(int response_id)
+{
+  switch (response_id) {
+  case RESPONSE_FIND:	signal_textsubmit.emit(m_search_text.get_text(), m_case_sensitive.get_active() );
+			break;
+  default: 		break;
+  }
+}
---
+++ ickle/FindTextDialog.h
@@ -0,0 +1,54 @@
+/*
+ * FindTextDialog
+ * Copyright (C) 2001 Barnaby Gray <barnaby@beedesign.co.uk>.
+ * Copyright (C) 2003 Christian Borntraeger <linux@borntraeger.net>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef FINDTEXTDIALOG_H
+#define FINDTEXTDIALOG_H
+
+#include <gtkmm/dialog.h>
+#include <gtkmm/entry.h>
+#include <gtkmm/checkbutton.h>
+#include <gtkmm/tooltips.h>
+
+#include <sigc++/sigc++.h>
+
+class FindTextDialog : public Gtk::Dialog
+{
+ private:
+ Gtk::Entry m_search_text;
+ Gtk::Tooltips m_tooltip;
+ Gtk::CheckButton m_case_sensitive;
+ enum
+ {
+ RESPONSE_FIND=1
+ };
+
+ protected:
+  virtual void on_response(int response_id);
+
+ public:
+  FindTextDialog(Gtk::Window& parent, const Glib::ustring title, const Glib::ustring question,
+  const Glib::ustring oldsearch);
+  ~FindTextDialog();
+  SigC::Signal2<void,Glib::ustring,bool> signal_textsubmit;
+
+};
+
+#endif
